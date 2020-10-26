c1d25b07daf2a818908af6a8fd3d27114c23f261
Muhammad Usama <the.mgi@outlook.com>
2020-10-27 03:50:36 +0500

    Visual Changes

--- src/app/main-visualization-page/main-visualization-page.component.ts
+++ src/app/main-visualization-page/main-visualization-page.component.ts
@@ -1,9 +1,12 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnInit} from '@angular/core';
 import {
+    Bar,
     optionsAvailablePathfinding,
     optionsAvailableSearch,
     optionsAvailableSort,
-    optionsAvailableSpeedValues
+    optionsAvailableSpeedValues,
+    parseArray,
+    randomColor
 } from '../../sharedClasses/classTemplate';
 
 
@@ -12,7 +15,7 @@ import {
     templateUrl: './main-visualization-page.component.html',
     styleUrls: ['./main-visualization-page.component.css']
 })
-export class MainVisualizationPageComponent implements OnInit {
+export class MainVisualizationPageComponent implements OnInit, AfterViewChecked {
 
     optionsAvailableSearch = optionsAvailableSearch;
     optionsAvailableSort = optionsAvailableSort;
@@ -21,7 +24,11 @@ export class MainVisualizationPageComponent implements OnInit {
 
     ownArray: string;
     algorithm = 'Algorithm';
-    isDisabled = true;
+    isVisualizeButtonDisabled = false;
+
+    barsValue: Bar[] = [];
+    isBarsCreated = false;
+    number: number[] = [];
 
     constructor(private elementReference: ElementRef) {
     }
@@ -31,41 +38,51 @@ export class MainVisualizationPageComponent implements OnInit {
 
     setAlgorithmName(event: { nameAlgo: string }): void {
         this.algorithm = event.nameAlgo;
-        this.isDisabled = false;
     }
 
-    visualize(): void {
+    visualize(noOfBarsInput: HTMLInputElement, yourArrayInput: HTMLInputElement): void {
         const allBars = this.elementReference.nativeElement.querySelectorAll('.bars');
-        allBars.forEach(singleBar => {
-            console.log(singleBar);
+        console.log(`I am in visualize ${allBars.length}`);
+        const yourArray = yourArrayInput.value;
+        const numBars = noOfBarsInput.value;
+
+        this.barsValue = [];
+        this.createBars(yourArray);
+    }
+
+    createBars(yourArray: string): void {
+        this.number.push(2);
+        const array = parseArray(yourArray);
+        /*
+            if array = [1, 2, 3, 4]
+            then ...array = 1, 2, 3, 4, but only writing this will result in an error
+            ... is called as the spread operator, allowing the expression to be expanded
+         */
+        const maxValue = Math.max(...array);
+        array.forEach(singleValue => {
+            this.barsValue.push(new Bar(
+                singleValue,
+                (((singleValue / maxValue) * 100) * 50),
+                randomColor()
+                )
+            );
+        });
+        this.isBarsCreated = true;
+        this.linearSearch();
+    }
+
+    ngAfterViewChecked(): void {
+        const bars: HTMLElement[] = this.elementReference.nativeElement.querySelectorAll('.bars');
+        console.log('in linear search ' + bars.length);
+        bars.forEach(singleBar => {
+            setTimeout(() => {
+                console.log(singleBar.innerHTML);
+                console.log('I am iterating');
+            }, 100);
         });
     }
 
-    move(startPositionX: number, startPositionY: number, toPositionX: number, toPositionY: number): void {
-    }
+    linearSearch(): void {
 
-    selectionSort(): void {
     }
-
-    bubbleSort(): void {
-    }
-
-    quickSort(): void {
-    }
-
-    mergeSort(): void {
-    }
-
-    radixSort(): void {
-    }
-
-    bucketSort(): void {
-    }
-
-    bitonicSort(): void {
-    }
-
-    pancakeSort(): void {
-    }
-
 }
