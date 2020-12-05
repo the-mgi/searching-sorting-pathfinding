9b5605c3355b16198fbc8d29de1d5c8ea6f292b4
Muhammad Usama <the.mgi@outlook.com>
2020-12-05 15:34:04 +0500

    Removed HTML and CSS files

--- src/app/main-visualization-page/bar/bar.component.ts
+++ src/app/main-visualization-page/bar/bar.component.ts
@@ -2,8 +2,38 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-bar',
-    templateUrl: './bar.component.html',
-    styleUrls: ['./bar.component.css']
+    template: `
+        <div
+            class="single-bar"
+            [style.height.%]="height"
+            [style.backgroundColor]="color"
+            [ngClass]="{'current': isClassAttached}">
+            <p>{{valueToDisplay}}</p>
+        </div>`,
+    styles: [`.single-bar {
+        background-color: red;
+        width: 25px;
+        border-radius: 3px;
+        margin: 8px;
+        transition: .3s;
+    }
+
+    .single-bar p {
+        margin: 0;
+        background-color: black;
+        width: 25px;
+        height: 30px;
+        color: white;
+        font-size: 20px;
+        text-align: center;
+        font-family: Product-Sans-Regular, sans-serif;
+        border-radius: 3px;
+    }
+
+    .current {
+        box-shadow: 10px 10px 8px #585853;
+    }
+    `]
 })
 export class BarComponent implements OnInit {
 
@@ -14,8 +44,13 @@ export class BarComponent implements OnInit {
     private condition: boolean;
 
     @Input()
-    get isClassAttached(): boolean { return this.condition; }
-    set isClassAttached(condition: boolean) { this.condition = condition; }
+    get isClassAttached(): boolean {
+        return this.condition;
+    }
+
+    set isClassAttached(condition: boolean) {
+        this.condition = condition;
+    }
 
 
     constructor() {
