c1a034200ba4d67a530d4e9a533cec31d05cadaf
Muhammad Usama <the.mgi@outlook.com>
2020-12-05 15:25:44 +0500

    Removed associated un-necessary files

--- src/app/main-visualization-page/bar/bar.component.ts
+++ src/app/main-visualization-page/bar/bar.component.ts
@@ -2,39 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-bar',
-    template: `<div
-        class="single-bar"
-        [style.height.%]="height"
-        [style.backgroundColor]="color"
-        [ngClass]="{'current': isClassAttached}
-">
-        <p>{{valueToDisplay}}</p>
-    </div>
-    `,
-    styles: [`.single-bar {
-        background-color: red;
-        width: 25px;
-        border-radius: 3px;
-        margin: 8px;
-        transition: .3s;
-    }
-
-    .single-bar p {
-        margin: 0;
-        background-color: black;
-        width: 25px;
-        height: 30px;
-        color: white;
-        font-size: 20px;
-        text-align: center;
-        font-family: Product-Sans-Regular, sans-serif;
-        border-radius: 3px;
-    }
-
-    .current {
-        box-shadow: 10px 10px 8px #585853;
-    }
-    `]
+    templateUrl: './bar.component.html',
+    styleUrls: ['./bar.component.css']
 })
 export class BarComponent implements OnInit {
 
@@ -47,6 +16,7 @@ export class BarComponent implements OnInit {
     @Input()
     get isClassAttached(): boolean { return this.condition; }
     set isClassAttached(condition: boolean) { this.condition = condition; }
+
 
     constructor() {
     }
