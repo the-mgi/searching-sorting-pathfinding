6375a646c4641fd02fa33499857de03e31a837d7
Muhammad Usama <the.mgi@outlook.com>
2020-12-05 15:27:40 +0500

    Refactored

--- src/app/main-visualization-page/pathfinding/pathfinding.component.ts
+++ src/app/main-visualization-page/pathfinding/pathfinding.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {optionsAvailablePathfinding, optionsAvailableSpeedValues} from '../../../sharedClasses/classTemplate';
 
 @Component({
-  selector: 'app-pathfinding',
-  templateUrl: './pathfinding.component.html',
-  styleUrls: ['./pathfinding.component.css']
+    selector: 'app-pathfinding',
+    templateUrl: './pathfinding.component.html',
+    styleUrls: ['./pathfinding.component.css']
 })
 export class PathfindingComponent implements OnInit {
+    selectAlgorithm = 'Select Algorithm';
+    optionsAvailablePath = optionsAvailablePathfinding;
+    optionsAvailableSpeedValues = optionsAvailableSpeedValues;
+    algorithm = 'Algorithm';
+    speedToRun: number;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    setAlgorithmName(event: { nameAlgo: string }): void {
+        this.algorithm = event.nameAlgo;
+    }
+
+    updateSpeed(event: { speedToRun: string }): void {
+        this.speedToRun = +event.speedToRun.substr(0, event.speedToRun.length - 1); // because we have speeds like 1.5x
+        console.log(this.speedToRun);
+    }
+
+    startVisualization(): void {
+    }
 }
